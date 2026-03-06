5553843620576c59753c1a0f8881cfe07d57d90e
queentaamy <appiahasantewaa7@gmail.com>
2026-03-06 18:35:58 +0000

    Week 3 Part 2 Task 1: Python variables

---
+++ week3-python/string_practice.py
@@ -0,0 +1,12 @@
+
+# Task 1
+
+first_name = "Asantewa"
+last_name = "Appiah"
+age = 21
+favorite_concept = "Linux CLI"
+
+print(f"First Name: {first_name}")
+print(f"Last Name: {last_name}")
+print(f"Age: {age}")
+print(f"Favorite Concept: {favorite_concept}")
